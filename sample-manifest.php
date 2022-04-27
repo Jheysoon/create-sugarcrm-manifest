73e73092bdd42b9acbdbe377fa0ec428c278659f
Jayson Martinez <jlmartinez631@gmail.com>
2022-04-27 11:20:26 +0800

    Commit sample manifest file

---
+++ sample-manifest.php
@@ -0,0 +1,36 @@
+<?php
+
+$manifest = array(
+    'built_in_version' => '11.3.0',
+    'acceptable_sugar_versions' =>
+        array(
+            0 => '',
+        ),
+    'acceptable_sugar_flavors' =>
+        array(
+            0 => 'ENT',
+            1 => 'ULT',
+        ),
+    'readme' => '',
+    'key' => '',
+    'author' => '',
+    'description' => "",
+    'icon' => '',
+    'is_uninstallable' => true,
+    'name' => '',
+    'published_date' => '2022-04-27 11:30:00',
+    'type' => 'module',
+    'version' => '',
+    'remove_tables' => false,
+);
+
+$installdefs = array(
+    'id' => '',
+    'copy' =>
+        array(
+            array(
+                'from' => '<basepath>/',
+                'to' => ''
+            ),
+        ),
+);
